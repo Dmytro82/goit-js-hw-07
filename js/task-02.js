b48aa23edbedb8d8829ba6b04ea304a67f7f00b7
Dmytro82 <dmutro8228@gmail.com>
2021-09-14 21:46:31 +0300

    2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const ingredientsUl = document.querySelector('#ingredients');
+
+const element = [...ingredients].forEach(el => {
+  const Item = document.createElement('li');
+  Item.textContent = el;
+  ingredientsUl.appendChild(Item);
+});

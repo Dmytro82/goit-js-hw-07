e817fdbcdbf77eeddd667ed79ef9a49b75ba5363
Dmytro82 <dmutro8228@gmail.com>
2021-09-15 21:58:38 +0300

    3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,12 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+const imagesUl = document.querySelector('#gallery');
+
+images
+  .map(element => {
+    const picture = `<li><img src = "${element.url}" alt = "${element.alt}" width = 200 height = 150></li>`;
+    imagesUl.insertAdjacentHTML('afterbegin', picture);
+  })
+  .join('');
+imagesUl.setAttribute('style', 'list-style-type:none; display: flex;');

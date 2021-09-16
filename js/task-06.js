37a503ca1fc448b45e4df14d40e00fc462b025d0
Dmytro82 <dmutro8228@gmail.com>
2021-09-17 01:18:22 +0300

    6

---
+++ js/task-06.js
@@ -0,0 +1,12 @@
+const inputRef = document.querySelector('#validation-input');
+
+function checkLength() {
+  if (inputRef.value.length === Number(inputRef.dataset.length)) {
+    inputRef.classList.remove('invalid');
+    inputRef.classList.add('valid');
+  } else {
+    inputRef.classList.remove('valid');
+    inputRef.classList.add('invalid');
+  }
+}
+inputRef.addEventListener('focus', checkLength);
